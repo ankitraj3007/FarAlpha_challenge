8093d63328f04f8ba90ae5f7129d5a72a0d647ec
Ubuntu <azureuser@challenge-9.wemevqvv5vtedjrrpqrwhg5rhf.bx.internal.cloudapp.net>
2024-08-18 11:31:53 +0000

    Ankit Raj server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+
+app.get('/sayHello',(req,res)=>{
+res.json({message:"Hello,User"})
+});
+
+app.listen(80,()=>{
+console.log('Server running on port 80');
+});
+
+
